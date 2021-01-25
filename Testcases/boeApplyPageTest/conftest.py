86512f033340c4734a4a819e5774da18bef2b600
huangchao <17600445140@163.com>
2021-01-25 08:48:43 +0800

    2021/1/25 success

---
+++ Testcases/boeApplyPageTest/conftest.py
@@ -0,0 +1,42 @@
+from Util import loadTestData
+
+import pytest
+import os
+
+suitename = {
+    "test_comFeeApplyBoe": 'comFeeApplyBoe',
+    "test_applyTravelBoe": 'applyTravelBoe',
+    "test_applyInternationalTravelBoe": 'applyInternationalTravelBoe'
+}
+
+
+comFeeApplyBoe_data,comFeeApplyBoe_casename = loadTestData.get_ymltestdata(
+    loadTestData.get_ymldatapath(os.path.dirname(__file__)),suitename["test_comFeeApplyBoe"])
+@pytest.fixture(params=comFeeApplyBoe_data,ids=comFeeApplyBoe_casename,scope="class")
+def comFeeApplyBoe_testdata(request) ->dict:
+    # 返回数据
+    return request.param
+
+
+applyTravelBoe_data,applyTravelBoe_casename = loadTestData.get_ymltestdata(
+    loadTestData.get_ymldatapath(os.path.dirname(__file__)),suitename["test_applyTravelBoe"])
+@pytest.fixture(params=applyTravelBoe_data,ids=applyTravelBoe_casename,scope="class")
+def applyTravelBoe_testdata(request) ->dict:
+    # 返回数据
+    return request.param
+
+
+applyInternationalTravelBoe_data,applyInternationalTravelBoe_casename = loadTestData.get_ymltestdata(
+    loadTestData.get_ymldatapath(os.path.dirname(__file__)),suitename["test_applyInternationalTravelBoe"])
+@pytest.fixture(params=applyInternationalTravelBoe_data,ids=applyInternationalTravelBoe_casename,scope="class")
+def applyInternationalTravelBoe_testdata(request) ->dict:
+    # 返回数据
+    return request.param
+
+
+
+if __name__ == '__main__':
+    # print(managementPage_data)
+    # print(managementPage_casename)
+    print(loadTestData.get_ymltestdata(
+    loadTestData.get_ymldatapath(os.path.dirname(__file__)),suitename["test_applyTravelBoe"]))
