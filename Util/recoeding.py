2443917ca4a47b7ad37a0c4e0dcd6bf5bf67816a
huangchao <17600445140@163.com>
2021-01-26 17:26:23 +0800

    2021/1/26 增加记录文件，用力上下文用例传递

---
+++ Util/recoeding.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+import json
+import os
+
+from Util import config, logger
+
+
+class Recording(object):
+
+    def __init__(self):
+        pass
+
+    # 获取 record.json 文件目录
+    @property
+    def _getRecordingPath(self):
+        return os.path.join(config.testDataPath(), 'localMemory', 'record.json')
+
+    # 写入记录文件
+    def writeDataToRecord(self, dict, type='boeData'):
+        with open(self._getRecordingPath, "r", encoding="UTF-8") as f:
+            rData = json.load(f)
+        rData[type].update(dict)
+        data = rData
+        with open(self._getRecordingPath, "w", encoding="UTF-8") as f:
+            json.dump(data, f)
+        logger.debug('记录文件信息, 文件信息为 : {}'.format(dict))
+
+    # 读取记录文件
+    def readDataFromRecord(self, type='boeData'):
+        with open(self._getRecordingPath, "r", encoding="UTF-8") as f:
+            rData = json.load(f)
+        return rData[type]
+
+
+record = Recording()
+
+if __name__ == '__main__':
+    print(record.writeDataToRecord())
