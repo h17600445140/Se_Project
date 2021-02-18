3ace7e0c04055b9d6d2c8fb45058bdcfc0a429e6
huangchao <17600445140@163.com>
2021-02-18 08:50:10 +0800

    2021/2/18

---
+++ Testcases/basIndexPageTest/salaryEasPageTest/test_newAddSalary.py
@@ -0,0 +1,59 @@
+# -*- coding:utf-8 -*-
+from time import sleep
+
+import allure
+import pytest
+
+from PageClass.basIndexPageClass.salaryEasPageClass.newAddSalaryPage import NewAddSalaryPage
+from Testcases.common.loginDepend import LoginDepend
+from Util import logger
+
+
+@allure.feature("新增薪酬流程")
+class TestNewAddSalary():
+
+    boeNum = globals()
+
+    def setup_class(self):
+        self.publicLogin = LoginDepend('basHost', 'user')
+        self.newAddSalaryPage = NewAddSalaryPage(self.publicLogin.driver)
+
+    def teardown_class(self):
+        # self.newAddSalaryPage.driver.quit()
+        pass
+
+    @allure.story("新增薪酬业务报账界面流程")
+    @allure.step("新增薪酬业务报账界面流程")
+    @allure.severity("blocker")
+    @pytest.mark.dependency(name='submit')
+    def test_newAddSalary(self):
+
+        logger.info(" ----- 新增薪酬流程开始 ----- ")
+
+        with allure.step("点击选择薪酬报账页面"):
+            self.newAddSalaryPage.selectTabType('薪酬报账')
+        with allure.step("进入薪酬报账新增提交页面"):
+            self.newAddSalaryPage.boeRntry('新增薪酬')
+
+        global boeNum
+        boeNum = self.newAddSalaryPage.getBoeNum()
+
+        with allure.step("选择薪酬区间"):
+            self.newAddSalaryPage.selectSalaryPeriod('2021-2')
+        with allure.step("选择业务类型"):
+            self.newAddSalaryPage.selectSalaryOperationType('工资计提')
+        with allure.step("输入备注"):
+            self.newAddSalaryPage.input_salaryRemark('测试')
+        with allure.step("点击保存按钮"):
+            self.newAddSalaryPage.click_salarySaveButton()
+
+        assert self.newAddSalaryPage.getToastBoxText() == '保存成功'
+
+        with allure.step("点击明细新增按钮"):
+            self.newAddSalaryPage.click_salaryAddButton()
+        with allure.step("选择责任部门"):
+            self.newAddSalaryPage.selectSalaryDept('hcCBZX', deptName='hc成本中心A部门')
+        with allure.step("输入薪酬明细"):
+            self.newAddSalaryPage.input_JiTiShiFa('100.00')
+            # self.newAddSalaryPage.input_JiTiYingFa('90')
+            # self.newAddSalaryPage.input_JiTiKouKuan('10')
